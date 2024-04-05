291908b5ad60ff2a1789e31c1ccbdaad657b6da8
lellithan <faulhaberellie1@gmail.com>
2024-04-05 15:42:56 -0700

    created new program that is a welcome screen

---
+++ assignment140/assignment140/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace assignment140
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Acme Accounting Sytems\nRemember, we're \"accounting\" on you!");
+            Console.ReadLine();
+        }
+    }
+}
