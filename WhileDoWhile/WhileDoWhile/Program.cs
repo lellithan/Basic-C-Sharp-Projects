e3779e6fdefbe5aa790142a5ef1f271f73792988
lellithan <faulhaberellie1@gmail.com>
2024-04-06 22:54:55 -0700

    created an app that uses both a while statement and a do while statement using boolean comparisons

---
+++ WhileDoWhile/WhileDoWhile/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileDoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;  //  create variable i
+            while (i < 10) //   while i is less than 10 print it to the console.
+            {
+                Console.WriteLine(i);
+                i++; // increment i
+            }
+
+            do
+            {
+                Console.WriteLine(i); // do ensures that that i will be incremented and printed at least once
+                i--;
+            }
+            while (i >= 0);
+
+            Console.ReadLine();
+        }
+    }
+}
