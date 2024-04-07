1230e864e399a3c5abaa9847e0541f7b9b65b5b2
lellithan <faulhaberellie1@gmail.com>
2024-04-06 23:18:34 -0700

    string assignment, concatenate 3 strings and utilize a stringbuilder

---
+++ StringAssignment/StringAssignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create 3 strings to concatenate together
+            string firstName = "Ellie",
+                middleName = "J",
+                lastName = "Faulhaber";
+            //  print to the console my full name and then make a new line and print my first name in all uppercase
+            Console.WriteLine("Full name: " + firstName + " " + middleName + " " + lastName +
+                "\nUppercase first name: " + firstName.ToUpper()
+                );
+
+            //  create a new instance of a stringbuilder
+            StringBuilder sb = new StringBuilder();
+
+            //  append 3 different string one by one onto our stringbuilder
+            sb.Append("Picking up twigs, ");
+            sb.Append("picking up twigs, ");
+            sb.Append("picking up, picking up, picking up twigs.");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StringFunction/StringFunction/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "Ellie";
+            //string quote = "The man said, \"Hello\", Ellie.\nHello on a new line.\n\tHello on a tab.";
+            //string fileName = @"C:\Users\Ellie";
+
+            //bool trueOrFalse = name.Contains("s");
+            //trueOrFalse = name.EndsWith("s");
+
+            //int length = name.Length;
+
+            //name = name.ToUpper();
+
+            //Console.WriteLine(name);
+
+            //string name = "Ellie";
+            //name = "Smellie";
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("My name is Ellie.");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
