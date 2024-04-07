7ca0b4efd7a9ec3dacf418e0721642866484b393
lellithan <faulhaberellie1@gmail.com>
2024-04-06 19:58:41 -0700

    created two new projects to follow the video and complete the new assignment.

---
+++ MathAndOperations/MathAndOperations/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathAndOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number.");
+            double userNumber = Convert.ToDouble(Console.ReadLine()); // take in a string from the user and convert to double
+            Console.WriteLine("Operations on your number:\n * 50 = " + (userNumber * 50) + //start performing the operations on our number - first multiply
+                "\n + 25 = " + (userNumber + 25) + // add 25 to it
+                "\n / 12.5 = "  + (userNumber / 12.5) + // divide it by 12.5
+                "\n > 50 = " + (userNumber > 50) + // comparison operator that returns true or false
+                "\n % 7 = " + (userNumber % 7) // find the modulus of the number divided by 7
+                );
+            Console.ReadLine();
+        }
+    }
+}
+
--- assignment140/assignment140/Program.cs
+++ assignment140/assignment140/Program.cs
@@ -8,6 +8,19 @@ namespace assignment140
         {
             Console.WriteLine("Welcome to Acme Accounting Sytems\nRemember, we're \"accounting\" on you!");
             Console.ReadLine();
+
+            int eight = 8;
+            int five = 5;
+
+            // addition
+            Console.WriteLine("Add: " + (eight + five) + 
+                "\nSubtract: " + (eight - five) +
+                "\nMultiply: " + (eight * five) +
+                "\nDivide: " + (eight / five)
+                );
+            Console.ReadLine();
+
+            
         }
     }
 }
