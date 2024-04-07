124a62df092a0db91930a1b0f69f23933e547c62
lellithan <faulhaberellie1@gmail.com>
2024-04-06 21:34:10 -0700

    updates to comments

--- CarInsuranceQualify/CarInsuranceQualify/Program.cs
+++ CarInsuranceQualify/CarInsuranceQualify/Program.cs
@@ -6,15 +6,19 @@ namespace CarInsuranceQualify
     {
         static void Main(string[] args)
         {
-            //  print to the console to see if 
-            Console.WriteLine("Let's check to see if you qualify for our car insurance!\nTo start, what is your age?");
+            //  print to the console to tell the user to answer some information
+            Console.WriteLine("Let's check to see if you qualify for our car insurance!\nTo start, what is your age?"); //  applicant age
             ushort age = Convert.ToUInt16(Console.ReadLine());
-            Console.WriteLine("Have you ever had a DUI? answer \"true\" or \"false.\"");
+            Console.WriteLine("Have you ever had a DUI? answer \"true\" or \"false.\""); // if applicant has ever had a dui and store in a bool
             bool dui = Convert.ToBoolean(Console.ReadLine());
-            Console.WriteLine("How many speeding tickets do you have?");
+            Console.WriteLine("How many speeding tickets do you have?"); // ask the applicant how many speedtickets they have received
             ushort ticketCount = Convert.ToUInt16(Console.ReadLine());
 
-            bool qualifed = (age > 15 && dui == false && ticketCount < 3);
+            //  three qualifiers that the applicant must succeed in
+            //  1. applicant must be older than 15 years of age
+            //  2. applicant must not have any duis
+            //  3. applicant must not have more than 3 speeding tickets
+            bool qualifed = (age > 15 && dui == false && ticketCount <= 3);
             Console.WriteLine("Qualified?\n" + qualifed);
             Console.ReadLine();
         }
