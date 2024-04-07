db3be138a9f014c236ea7a7032d4a77a2fd16237
lellithan <faulhaberellie1@gmail.com>
2024-04-07 00:01:02 -0700

    created a program that creates arrays and lists and then lets the user pick an article by giving it an index

---
+++ ArrayAndLists/ArrayAndLists/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Program
+{
+    static void Main()
+    {
+        //int[] numArray = new int[5];
+        //numArray[0] = 5;
+        //numArray[1] = 2;
+        //numArray[2] = 10;
+        //numArray[3] = 200;
+        //numArray[4] = 5000;
+
+        //int[] numArray1 = new int[] { 5, 2, 1, 200, 5000 };
+
+        //int[] numArray2 = { 5, 2, 1, 200, 5000, 600, 2300 };
+
+        //numArray2[5] = 650;
+
+        //Console.WriteLine(numArray2[5]);
+
+        //Console.WriteLine(numArray[3]);
+
+        List<string> intList = new List<string>();
+        intList.Add("Ellie");
+        intList.Add("Kelsey");
+        intList.Remove("Kelsey");
+
+        Console.WriteLine(intList[0]);
+        Console.ReadLine();
+    }
+}
---
+++ ArrayAndListsAssignment/ArrayAndListsAssignment/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArrayAndListsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  create a string array and show the array to the user and then get an index variable from the user of which name they want to select
+            string[] stringArray = { "Ellie", "Kelsey", "Alex", "Faron", "Nicole", "Carmen" };
+            Console.WriteLine("Welcome to Data Collections Oregon!\nPick out a name using an indexed number! Starting with 0.");
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                Console.WriteLine(stringArray[i]);
+            }
+
+            //  get the index from the user
+            int index = Convert.ToInt32(Console.ReadLine());
+
+            //  if the index that the user gives is out of range of our array then we inform the user and move to the next array
+            if (index > stringArray.Length || index < 0)
+            {
+                Console.WriteLine("Index you entered is not within range of the array.");
+            }
+            else
+            {
+                Console.WriteLine(stringArray[index]);
+            }
+            
+            //  start again but this time we use an integer array
+            int[] intArray = { 1, 5, 50, 100, 105, 210 };
+            Console.WriteLine("Pick out a integer using an indexed number! Starting with 0.");
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                Console.WriteLine(intArray[i]);
+            }
+
+            //  get the index
+            index = Convert.ToInt32(Console.ReadLine());
+            
+            //  display an error message if needed
+            if (index > intArray.Length || index < 0)
+            {
+                Console.WriteLine("Index you entered is not within range of the array.");
+            }
+            else
+            {
+                Console.WriteLine(intArray[index]);
+            }
+
+            //  create a list of data type strings and display it to the user
+            List<string> stringList = new List<string>() { "Zero", "One", "Two", "Three", "Four", "Five" };
+            for (int i = 0; i < stringList.Count; i++)
+            {
+                Console.WriteLine(stringList[i]);
+            }
+
+            //  get the indexed string that the user wants
+            index = Convert.ToInt32(Console.ReadLine());
+
+            // display error message if necessary
+            if (index > stringList.Count || index < 0)
+            {
+                Console.WriteLine("Index you entered is not within range of the list.");
+            }
+            else
+            {
+                // if error is not flagged then print the indexed article
+                Console.WriteLine(stringList[index]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
