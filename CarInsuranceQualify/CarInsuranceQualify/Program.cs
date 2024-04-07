d39b148691a14c32f6031bdc682ca43a6db7c9e7
lellithan <faulhaberellie1@gmail.com>
2024-04-06 21:30:14 -0700

    created a program to check if an applicant qualifies for car insurance

---
+++ CarInsuranceQualify/CarInsuranceQualify/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CarInsuranceQualify
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  print to the console to see if 
+            Console.WriteLine("Let's check to see if you qualify for our car insurance!\nTo start, what is your age?");
+            ushort age = Convert.ToUInt16(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI? answer \"true\" or \"false.\"");
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have?");
+            ushort ticketCount = Convert.ToUInt16(Console.ReadLine());
+
+            bool qualifed = (age > 15 && dui == false && ticketCount < 3);
+            Console.WriteLine("Qualified?\n" + qualifed);
+            Console.ReadLine();
+        }
+    }
+}
