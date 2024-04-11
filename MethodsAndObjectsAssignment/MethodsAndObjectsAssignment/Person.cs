891049a5a4434e87322bfbb92770c5726a8e3b41
lellithan <faulhaberellie1@gmail.com>
2024-04-11 15:25:19 -0700

    created two classes. one parent, one child. instatiate the child and make call to method of parent class

---
+++ MethodsAndObjectsAssignment/MethodsAndObjectsAssignment/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjectsAssignment
+{
+    public class Person
+    {
+        public void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
+        public string FirstName;
+        public string LastName;
+    }
+}
---
+++ MethodsAndObjectsAssignment/MethodsAndObjectsAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjectsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiate class employee
+            Employee employee = new Employee();
+            // set values to the empty string variables from the person class - inheritance
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+            // because employee is child class of person, we can call method SayName() - inheritance
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
