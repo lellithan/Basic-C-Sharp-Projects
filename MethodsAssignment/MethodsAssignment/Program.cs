56881e74be23d65457eae1ea9bb783c6a0503905
lellithan <faulhaberellie1@gmail.com>
2024-04-10 23:19:36 -0700

    created a short program that has a class which operates on an integer whose value is decided by the user

---
+++ MethodsAssignment/MethodsAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create a new class that has an int that will be the userInput that we store so we can do operations on
+            byFive letsOperate = new byFive();
+            // ask the user for a number and then store it within our new class object and operate on it
+            Console.WriteLine("Enter a number that we will operate on.");
+            letsOperate.userInput = Convert.ToInt32(Console.ReadLine());
+            // simultaneously print the operation results and call the classes methods that return the result of the two numbers
+            Console.WriteLine("Your number times 5 is equal to: " + letsOperate.timesFive(letsOperate.userInput) +
+                "\nYour number plus 5 is equal to: " + letsOperate.plusFive(letsOperate.userInput) +
+                "\nYour number minus 5 is equal to: " + letsOperate.minusFive(letsOperate.userInput));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MethodsAssignment/MethodsAssignment/byFive.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAssignment
+{
+    public class byFive
+    {
+        public byFive() { userInput = 0; }
+        public int timesFive(int user)
+        {
+            return userInput * 5;
+        }
+        public int plusFive(int user)
+        {
+            return userInput + 5;
+        }
+        public int minusFive(int user)
+        {
+            return userInput - 5;
+        }
+        public int userInput;
+    }
+}
