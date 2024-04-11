92f832cf7746ac4844bd09b360fa2359da51e45c
lellithan <faulhaberellie1@gmail.com>
2024-04-11 14:05:00 -0700

    updated the main to instead instatiate the user input to a variable within the class and just directly passing it into the class method. removed numOne and numTwo in the class and deleted the constructor

--- MethodAssignment/MethodAssignment/Program.cs
+++ MethodAssignment/MethodAssignment/Program.cs
@@ -15,19 +15,19 @@ namespace MethodAssignment
             // print to user that we need two numbers and the second one is not required 
             Console.WriteLine("Enter two numbers, press enter after each number.\nThe second number is not necessary, leave it blank by pressing enter if you would like.");
             // since the first number is required then we convert it and store it as an integer
-            optional.numOne = Convert.ToInt32(Console.ReadLine());
+            int numOne = Convert.ToInt32(Console.ReadLine());
             // the second input we are going to store as the string version because we need to first check if the user actually input anything
-            string user = Console.ReadLine();
+            string inputTwo = Console.ReadLine();
             // if the user entered nothing for the second input then we run the else statement which will use the defaulted argument of the funcion.
             // if it isnt empty then pass the number entered to the second parameter of the function.
-            if (user != "")
+            if (inputTwo != "")
             {
-                optional.numTwo = Convert.ToInt32(user);
-                Console.WriteLine("Number one + (number two * 5) is: " + optional.timesFive(optional.numOne, optional.numTwo));
+                int numTwo = Convert.ToInt32(inputTwo);
+                Console.WriteLine("Number one + (number two * 5) is: " + optional.timesFive(numOne, numTwo));
             }
             else
             {
-                Console.WriteLine("Number one + (number two * 5) is: " + optional.timesFive(optional.numOne));
+                Console.WriteLine("Number one + (number two * 5) is: " + optional.timesFive(numOne));
             }
             Console.ReadLine();
         }
--- MethodAssignment/MethodAssignment/optionalInt.cs
+++ MethodAssignment/MethodAssignment/optionalInt.cs
@@ -8,12 +8,9 @@ namespace MethodAssignment
 {
     class optionalInt
     {
-        public optionalInt() { numOne = 0; numTwo = 0; }
         public int timesFive(int num, int times = 0) 
         {
             return num + (5 * times);
         }
-        public int numTwo;
-        public int numOne;
     }
 }
