755a461e104d0050447dfd9606aa782704b94e62
lellithan <faulhaberellie1@gmail.com>
2024-04-11 16:46:21 -0700

    person base and employee child using abstract methods and override

---
+++ AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiate a new variable of type employee
+            Employee employee = new Employee();
+            // we inherit from person so we can instaniate the public variables from main
+            employee.firstName = "Sample";
+            employee.lastName = "Student";
+            // make a call to the specific SayName() function that is within child class employee
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -10,35 +10,20 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-            Game game = new Game();
-            game.Players = new List<string> { "Ellie", "Kelsey", "Wadey" };
+            TwentyOneGame game = new TwentyOneGame();
+            game.Players = new List<string>() { "Ellie", "Alex", "Carmen" };
             game.ListPlayers();
             Console.ReadLine();
 
+            Deck deck = new Deck();
+            deck.Shuffle(3);
 
-
-            
-        //    Deck deck = new Deck();
-
-        //    deck.Shuffle(3);
-
-        //    foreach (Card card in deck.Cards)
-        //    {
-        //        Console.WriteLine(card.Face + " of " + card.Suit);
-        //    }
-        //    Console.WriteLine(deck.Cards.Count);
-        //    Console.ReadLine();
-        //}
-
-        
-
-        //public static Deck Shuffle(Deck deck, int times)
-        //{
-        //    for (int i = 0; i < times; i++)
-        //    {
-        //        Shuffle(deck);
-        //    }
-        //    return deck;
+            foreach (Card card in deck.Cards)
+            {
+                Console.WriteLine(card.Face + " of " + card.Suit);
+            }
+            Console.WriteLine(deck.Cards.Count);
+            Console.ReadLine();
         }
     }
 }
