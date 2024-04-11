b62725be15d8caef207949eaa20fabbdc99e381a
lellithan <faulhaberellie1@gmail.com>
2024-04-10 23:51:47 -0700

    created a simple class that has overloaded functions that have different parameters but the same function

---
+++ MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create a new class which has three overloaded funcitons, one that takes a string,
+            // one that takes an int, and one that takes a decimal and returns a decimal
+            overloadedAddition addition = new overloadedAddition();
+            Console.WriteLine("The sum of 26 + 8 is: " + addition.Addition(8) +
+                "\nThe sum of 26 + 3.452 is: " + addition.Addition(3.452m) + 
+                "\nThe sum of 26 + 3 is: " + addition.Addition("3"));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MainMethodAssignment/MainMethodAssignment/overloadedAddition.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class overloadedAddition
+    {
+        // default constructor sets number to 26
+        public overloadedAddition()
+        {
+            number = 26;
+        }
+        // over loaded functions that takes and returns an int
+        public int Addition(int num)
+        {
+            return number + num;
+        }
+        // overloaded function that takes a decimal and returns a decimal
+        public decimal Addition(decimal num)
+        {
+            return Convert.ToDecimal(number) + num;
+        }
+        // and finally, a function that takes in a string and returns an int
+        public int Addition(string num)
+        {
+            int sum = 0;
+            try
+            {
+                sum = Convert.ToInt32(num) + number;
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("String was not a convertable number.");
+                Console.ReadLine();
+            }
+            return sum;
+        }
+        public int number;
+    }
+}
