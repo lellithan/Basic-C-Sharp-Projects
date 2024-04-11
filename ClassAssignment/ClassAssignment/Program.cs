4896b685ed8b43e0df12bc059978c5d458a93737
lellithan <faulhaberellie1@gmail.com>
2024-04-11 14:54:13 -0700

    created a class with an overloaded void method. one with an out and another that is static and without an out.

---
+++ ClassAssignment/ClassAssignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // step 2 - instantiate the class
+            divideTwo divide = new divideTwo();
+            Console.WriteLine("Enter a whole number: ");
+            // ask the user for input to divide by
+            int userInput;
+            int userInputTwo = Convert.ToInt32(Console.ReadLine());
+
+            // overloaded methods. one that has an out and one does not. one is static and the other is not.
+            // because the operate has an out we need to create a variable that we are going to instatiate
+            // within the method and output it to its place in memory here.
+            divideTwo.Operate(userInputTwo);
+            // becasue this method is static we instead make a call to it by the class type. not the instance of divide decalared earlier
+            divide.Operate(out userInput);
+            Console.WriteLine(userInput);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ClassAssignment/ClassAssignment/divideTwo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment
+{
+    // step 1 - created a class that has a void int that outputs an int
+    class divideTwo
+    {
+        public static void Operate(int num)
+        {
+            Console.WriteLine("The num passed in divided by 2 is: " + num / 2);
+        }
+        public void Operate(out int num)
+        {
+            Console.WriteLine("Please enter another whole number.");
+            num = Convert.ToInt32(Console.ReadLine()) / 2;
+        }
+    }
+}
