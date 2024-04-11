f1fb3406012a0609d9fcc3ea5be16b8062608f06
lellithan <faulhaberellie1@gmail.com>
2024-04-11 14:16:14 -0700

    created a void method within a class and made a call to it twice within the main program

---
+++ MethodClassAssignment/MethodClassAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiate the new class that has our void method
+            voidMethod runMethod = new voidMethod();
+
+            // call the method within, while passing in two ints
+            runMethod.Operation(3, 7);
+
+            // instatiate two new int variables and then calling the method and passing in the arguments by name
+            int numOne = 3;
+            int numTwo = 7;
+            runMethod.Operation(numOne, numTwo);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MethodClassAssignment/MethodClassAssignment/voidMethod.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class voidMethod
+    {
+        public void Operation(int intOne, int intTwo)
+        {
+            // perform an operation on the first argument
+            intOne += 7;
+            // print the second argument to the screen
+            Console.WriteLine("Var intOne is: " + intTwo);
+        }
+    }
+}
