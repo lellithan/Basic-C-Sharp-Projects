d9997ba0a5a74cf12afe4da23e33e1b16730b225
lellithan <faulhaberellie1@gmail.com>
2024-04-11 00:27:44 -0700

    created a class that has a function with a completely optional parameter

---
+++ MethodAssignment/MethodAssignment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create our class that has the method we need to use to operate on
+            optionalInt optional = new optionalInt();
+            // print to user that we need two numbers and the second one is not required 
+            Console.WriteLine("Enter two numbers, press enter after each number.\nThe second number is not necessary, leave it blank by pressing enter if you would like.");
+            // since the first number is required then we convert it and store it as an integer
+            optional.numOne = Convert.ToInt32(Console.ReadLine());
+            // the second input we are going to store as the string version because we need to first check if the user actually input anything
+            string user = Console.ReadLine();
+            // if the user entered nothing for the second input then we run the else statement which will use the defaulted argument of the funcion.
+            // if it isnt empty then pass the number entered to the second parameter of the function.
+            if (user != "")
+            {
+                optional.numTwo = Convert.ToInt32(user);
+                Console.WriteLine("Number one + (number two * 5) is: " + optional.timesFive(optional.numOne, optional.numTwo));
+            }
+            else
+            {
+                Console.WriteLine("Number one + (number two * 5) is: " + optional.timesFive(optional.numOne));
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MethodAssignment/MethodAssignment/optionalInt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    class optionalInt
+    {
+        public optionalInt() { numOne = 0; numTwo = 0; }
+        public int timesFive(int num, int times = 0) 
+        {
+            return num + (5 * times);
+        }
+        public int numTwo;
+        public int numOne;
+    }
+}
