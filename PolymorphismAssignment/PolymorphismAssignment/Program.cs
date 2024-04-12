6a498b675a829266215945dded8d6ba1c1f49f19
lellithan <faulhaberellie1@gmail.com>
2024-04-11 17:04:54 -0700

    comments

--- PolymorphismAssignment/PolymorphismAssignment/Program.cs
+++ PolymorphismAssignment/PolymorphismAssignment/Program.cs
@@ -10,7 +10,9 @@ namespace PolymorphismAssignment
     {
         static void Main(string[] args)
         {
+            // create a polymorphic instance of IQuittable using new Employee child class
             IQuittable quit = new Employee();
+            // make call to employee method Quit()
             quit.Quit();
             Console.ReadLine();
         }
