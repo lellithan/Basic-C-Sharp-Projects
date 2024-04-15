3341cccc23a5db06ef68ee6a8794ebdbdba8c18f
lellithan <faulhaberellie1@gmail.com>
2024-04-14 18:38:25 -0700

    created a try catch program. brain fried. help.

---
+++ TryCatchAssignment/TryCatchAssignment/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! Input you or someone else's age to the console.\nWe'll give you the year you/they were born.");
+
+            int userAge;
+            int currentYear = 2024;
+
+            try
+            {
+                userAge = Convert.ToInt32(Console.ReadLine());
+                isPositive(userAge);
+            }
+            catch (NegativeOrZeroException)
+            {
+                Console.WriteLine("Zero and negative numbers are not allowed. Please enter positive whole integers.");
+                Console.ReadLine();
+                return;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please try again. Please enter positive whole integers.");
+                Console.ReadLine();
+                return;
+            }
+            Console.WriteLine("The person's age you entered was born in the year " + (currentYear - userAge));
+            Console.ReadLine();
+        }
+        public static void isPositive(int age)
+        {
+            if (age <= 0)
+            {
+                throw new NegativeOrZeroException();
+            }
+        }
+    }
+}
--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -13,8 +13,16 @@ namespace TwentyOne
 
             Console.WriteLine("Welcome to the Grand Hotel and Casino. Let's start by telling me your name.");
             string playerName = Console.ReadLine();
-            Console.WriteLine("And how much money did you bring today?");
-            int bank = Convert.ToInt32(Console.ReadLine());
+
+            bool validAnswer = false;
+            int bank = 0;
+            while (!validAnswer)
+            {
+                Console.WriteLine("And how much money did you bring today?");
+                validAnswer = int.TryParse(Console.ReadLine(), out bank);
+                if (!validAnswer) Console.WriteLine("Please enter digits only, no decimals.");
+            }
+
             Console.WriteLine("Hello, {0}. Would you like to join a game of 21 right now? ", playerName);
             string answer = Console.ReadLine().ToLower();
 
@@ -31,7 +39,23 @@ namespace TwentyOne
                 player.isActivelyPlaying = true;
                 while (player.isActivelyPlaying && player.Balance > 0)
                 {
-                    game.Play();
+                    try
+                    {
+                        game.Play();
+                    }
+                    catch (FraudException)
+                    {
+                        Console.WriteLine("Security! Kick this person out.");
+                        Console.ReadLine();
+                        return;
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("An error occurred. Please contact your System Administrator.");
+                        Console.ReadLine();
+                        return;
+                    }
+                    
                 }
                 game -= player;
                 Console.WriteLine("Thank you for playing!");
@@ -40,5 +64,10 @@ namespace TwentyOne
             Console.WriteLine("Feel free to look around the casino. Bye for now.");
             Console.ReadLine();
         }
+        private static void UpdateDbWithException(Exception ex)
+        {
+            string connectionString = "Data Source=(localdb)\\ProjectsV13;Initial Catalog=TwentyOneGame;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        }
     }
+
 }
